fcfd613a0b202bc9ba82f45793d6991cdff9c415
bruno-german <brunogermansosapereira@gmail.com>
2025-06-04 08:43:46 -0300

    update gui

--- gui.py
+++ gui.py
@@ -1,11 +1,16 @@
 import tkinter as tk
 
-class HyperKeysGui:
-  def __init__(self):
-   pass
-  
-  def run(self):
-    root = tk.Tk()
-    root.geometry("500x400")
-    root.title('HyperKeys')
-    root.mainloop()
+class HyperKeysGui(tk.Frame):
+    def __init__(self, parent=None):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.init_ui()
+
+    def init_ui(self):
+        self.parent.title("HyperKeys")
+
+if __name__ == "__main__":
+    ROOT = tk.Tk()
+    ROOT.geometry("300x100")
+    APP = HyperKeysGui(parent=ROOT)
+    APP.mainloop()
--- main.py
+++
@@ -1,4 +0,0 @@
-import gui
-if __name__ == '__main__':
-    hk_gui = gui.HyperKeysGui()
-    hk_gui.run()
