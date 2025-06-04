1dff9fee1779d18e11c209cd30635f4a4ed918da
bruno-german <brunogermansosapereira@gmail.com>
2025-06-04 07:58:58 -0300

    create gui

---
+++ gui.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+class HyperKeysGui:
+  def __init__(self):
+   pass
+  
+  def run(self):
+    root = tk.Tk()
+    root.geometry("500x400")
+    root.title('HyperKeys')
+    root.mainloop()
